9660fac8a5c75fb60ccf21e5f21d01482a0c0369
alexei <dorofeev86@yahoo.com>
2021-01-01 11:15:24 +0100

    added new solution for 1486

--- src/new/3-1486.js
+++ src/new/3-1486.js
@@ -1,9 +1,17 @@
 const xorOperation = (n, start) => {
-  let arr = [];
+  let xor = 0;
   for (let i = 0; i < n; i++) {
-    arr[i] = start + 2 * i;
+    xor ^= start + 2 * i;
   }
-  return arr.reduce((a, c) => a ^ c);
+  return xor;
 };
+
+// const xorOperation = (n, start) => {
+//   let arr = [];
+//   for (let i = 0; i < n; i++) {
+//     arr[i] = start + 2 * i;
+//   }
+//   return arr.reduce((a, c) => a ^ c);
+// };
 
 console.log(xorOperation(1, 7));
