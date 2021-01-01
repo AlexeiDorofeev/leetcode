d83571f6ef934c80000668bc819bd9ef308e2208
alexei <dorofeev86@yahoo.com>
2021-01-01 09:57:07 +0100

    changed folder structure

---
+++ src/new/1-1603.js
@@ -0,0 +1,25 @@
+class ParkingSystem {
+  constructor(big, medium, small) {
+    this.slots = {
+      1: big,
+      2: medium,
+      et
+      3: small,
+    };
+  }
+
+  addCar(carType) {
+    if (this.slots[carType] === 0) {
+      return false;
+    }
+
+    this.slots[carType]--;
+    return true;
+  }
+}
+
+const parkingSystem = new ParkingSystem(1, 1, 0);
+console.log(parkingSystem.addCar(1));
+console.log(parkingSystem.addCar(2));
+console.log(parkingSystem.addCar(3));
+console.log(parkingSystem.addCar(1));
