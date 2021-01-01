35993b7723b48bcdd7ac46e938e60aa07bbf0f59
alexei <dorofeev86@yahoo.com>
2021-01-01 10:35:57 +0100

    1678

--- src/new/2-1678.js
+++ src/new/2-1678.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const interpret = (command) => {
+  return command.split("()").join("o").split("(al)").join("al");
+};
+
+console.log(interpret("G()()()()(al)"));
+
+// const interpret = (command) => {
+//   return command.replaceAll("()", "o").replaceAll("(al)", "al");
+// };
+
+// console.log(interpret("G()()()()(al)"));
